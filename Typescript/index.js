9af82fbe4ceed24a082a78418a0761dda166460f
Mean Says <oliveirafaelmoreno@gmail.com>
2023-01-22 20:50:51 -0300

    Typescript

---
+++ Typescript/index.js
@@ -0,0 +1,6 @@
+var firstName = "Rafael";
+var anotherNome = 1;
+function greating(name) {
+    console.log("Ola " + name);
+}
+greating(firstName);
---
+++ Typescript/index.ts
@@ -0,0 +1,13 @@
+const firstName = "Rafael";
+const anotherNome = "1";
+
+function greating(name : string) {
+    console.log("Ola " + name);
+
+}
+
+// sempre estar atento ao tipo da variavel criadar pois só sera executada quando for do tipo esperrado.
+
+
+greating(firstName)
+greating(anotherNome)
