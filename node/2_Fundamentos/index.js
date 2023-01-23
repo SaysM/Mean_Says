477ce136c8e0d3a1fb83a83a9f17045098b83947
Mean Says <oliveirafaelmoreno@gmail.com>
2023-01-23 02:42:35 -0300

    Node_2

---
+++ node/2_Fundamentos/index.js
@@ -0,0 +1,6 @@
+const meuModulo = require("./meu_modulo")
+
+const soma = meuModulo.soma
+
+soma(2,223)
+soma(5,25)
---
+++ node/2_Fundamentos/meu_modulo.js
@@ -0,0 +1,8 @@
+// para criar o proprio modulo e assim conseguir importar e exportar ele
+
+module.exports = {
+    soma(a, b){
+        console.log(a+b);
+    },
+}
+
