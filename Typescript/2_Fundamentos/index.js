65c41589967c2a4ee2cd65da0a64b9d1cff105ff
Mean Says <oliveirafaelmoreno@gmail.com>
2023-02-19 22:27:04 -0300

    Estudo de tipos

---
+++ Typescript/2_Fundamentos/index.js
@@ -0,0 +1,14 @@
+// 1 - numbers
+var r = 10;
+console.log(r);
+r = 16;
+console.log(typeof r);
+var y = 15.585858;
+console.log(y);
+console.log(y.toPrecision(3)); //toPrecision is for the quantification of the floating values
+// 2 - String
+var firstName = "Mean Says";
+console.log(firstName);
+console.log(firstName.toUpperCase);
+var fullname = firstName + "Oliveira Moreno Filho";
+console.log(fullname);
---
+++ Typescript/2_Fundamentos/index.ts
@@ -0,0 +1,26 @@
+// 1 - numbers
+let r: number = 10
+
+
+console.log(r)
+
+r = 16
+
+console.log(typeof r)
+
+const y:number = 15.585858  
+
+console.log(y)
+
+console.log(y.toPrecision(3)) //toPrecision is for the quantification of the floating values
+
+// 2 - String
+
+const firstName: string = "Mean Says"
+
+console.log(firstName)
+console.log(firstName.toUpperCase)
+
+let fullname: string = firstName + "Oliveira Moreno Filho"
+
+console.log(fullname)
